c00c9bac1685bb07aaf05d613defde1171d3034d
aalmeida9 <aaronjamesalmeida@gmail.com>
2021-04-23 13:27:57 -0700

    reorganize files and initialize input file

---
+++ DES.py
@@ -0,0 +1,10 @@
+# 16 character key/ciphertext
+
+# discard every 8th bit of key so 14 bits?
+
+# Start DES
+# Initial Permutation function on plain text
+
+# Initial permutation produces two halves; Left and right
+# Each half goes through 16 rounds of Encryption
+# ultimately havles are rejoined and a final permutation is done on the combined block
---
+++ bruteforce.py
@@ -0,0 +1,8 @@
+# ceasar cipher has a keyspace of 26 loop through the key space and
+# keep track of the key that has the most matches to the dictionary
+
+
+# birth year between 1970 (7B2) and 1985 (7C1)
+# input contains sender, receiver, encryption used, ciphertext
+# key space is the combination of 5 repetitions of the hex birthyear and
+# an additional character (0-F)
